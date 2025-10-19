0d1505bf65fbd7768bb9e93e170af31778ea51bd
Даня Арустамян <izrit@MacBook-Air-Dana.local>
2025-10-19 18:37:11 +0300

    .gitignore fix

--- 090125/build.sh
+++ 090125/build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 MAIN="main.cpp io.cpp sort.cpp"
 APP="example"
-if [ -f "$APP" ]; then rm "$APP"; fi
-g++ $MAIN -o $APP -std=c++17 -O2 -Wall -Wextra -Wpedantic -finput-charset=UTF-8 -fexec-charset=UTF-8
-./$APP
+BIN_DIR="bin"
+mkdir -p "$BIN_DIR"
+
+if [ -f "$BIN_DIR/$APP" ]; then
+    rm "$BIN_DIR/$APP"
+fi
+
+g++ $MAIN -o "$BIN_DIR/$APP" \
+    -std=c++17 -O2 -Wall -Wextra -Wpedantic \
+    -finput-charset=UTF-8 -fexec-charset=UTF-8
+
+./"$BIN_DIR/$APP"
